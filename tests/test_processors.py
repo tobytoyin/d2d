00b771a473321292792c54de50bcc0b9eb29874a
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-08-01 21:40:40 +0100

    update tests for include --- line breaks

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -4,15 +4,15 @@ from obsidian_py_processor.providers.obsidian.processors import *
 from tests.test_markdowns import *
 
 
-def test_frontmatter_processor_hv_meta(dummy_doc_with_frontmatter):
+def test_frontmatter_processor_hv_meta(doc_with_frontmatter):
     expected = {'id': 'd-123123', 'name': 'my-dummy-doc'}
-    result = frontmatter_processor(dummy_doc_with_frontmatter)
+    result = frontmatter_processor(doc_with_frontmatter)
     assert result == expected
 
 
-def test_frontmatter_processor_no_meta(dummy_doc_without_frontmatter):
+def test_frontmatter_processor_no_meta(doc_without_frontmatter):
     expected = {}
-    result = frontmatter_processor(dummy_doc_without_frontmatter)
+    result = frontmatter_processor(doc_without_frontmatter)
     assert result == expected
 
 
