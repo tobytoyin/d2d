9c8db33d3bc33a220c98b711b3256a7e7579e194
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-08-01 21:40:40 +0100

    update tests for include --- line breaks

--- tests/test_docs/doc_with_frontmatter.md
+++ tests/test_docs/doc_with_frontmatter.md
@@ -4,3 +4,11 @@ name: my-dummy-doc
 ---
 
 This is a dummy md
+
+
+---
+Line breaker shouldn't be extracted as meta data 
+
+---
+
+Another line
--- tests/test_markdowns.py
+++ tests/test_markdowns.py
@@ -3,13 +3,13 @@ import yaml
 
 
 @pytest.fixture
-def dummy_doc_with_frontmatter() -> str:
+def doc_with_frontmatter() -> str:
     with open('./tests/test_docs/doc_with_frontmatter.md', 'r') as f:
         text = f.read()
         return text
 
 @pytest.fixture
-def dummy_doc_without_frontmatter() -> str:
+def doc_without_frontmatter() -> str:
     with open('./tests/test_docs/doc_without_frontmatter.md', 'r') as f:
         text = f.read()
         return text
