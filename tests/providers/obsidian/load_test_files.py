cc1864127f8b17c3224c52ab7b8a587f2b4aa3e1
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-08-10 21:27:48 +0100

    update test strucutre

---
+++ tests/providers/obsidian/load_test_files.py
@@ -0,0 +1,26 @@
+import pytest
+import yaml
+
+from obsidian_py_processor.providers.obsidian.models import ObsidianDocument
+
+TEST_DOC_PREFIX = './tests/providers/obsidian'
+
+
+@pytest.fixture
+def doc_with_frontmatter() -> ObsidianDocument:
+    return ObsidianDocument(path=f'{TEST_DOC_PREFIX}/test_docs/doc_with_frontmatter.md')
+
+
+@pytest.fixture
+def doc_without_frontmatter() -> ObsidianDocument:
+    return ObsidianDocument(path=f'{TEST_DOC_PREFIX}/test_docs/doc_without_frontmatter.md')
+
+
+@pytest.fixture
+def doc_with_links() -> ObsidianDocument:
+    return ObsidianDocument(path=f'{TEST_DOC_PREFIX}/test_docs/doc_with_internal_links.md')
+
+
+@pytest.fixture
+def doc_without_links() -> ObsidianDocument:
+    return ObsidianDocument(path=f'{TEST_DOC_PREFIX}/test_docs/doc_without_internal_links.md')
