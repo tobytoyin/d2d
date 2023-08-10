f1e9655383768f091a0ceba15f5dd119870c6d41
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-08-10 21:33:06 +0100

    update relations models & tests

--- obsidian_py_processor/models.py
+++ obsidian_py_processor/models.py
@@ -1,83 +1,80 @@
 from abc import abstractmethod
-from typing import Callable
+from typing import Callable, Set
 
 from pydantic import BaseModel, ConfigDict
 
 RelationsProcessorFn = Callable[[str], set]
 MetadataProcessorFn = Callable[[str], dict]
 
+
 class DocumentMetadata(BaseModel):
     """Class to convert dict int a Metadataclass"""
+
     model_config = ConfigDict(extra='allow')
     doc_type: str
 
+
 class Document(BaseModel):
     relations_processor: RelationsProcessorFn = None
-    metadata_processor : MetadataProcessorFn = None
-    
+    metadata_processor: MetadataProcessorFn = None
+
     _contents: str = None  # str contents of the document
     _id: str = None
-    
+    _metadata: DocumentMetadata = None
+    _relations: Set[str] = None
+
     path: str  # source path of the document
-    
+
     @abstractmethod
     def read(self) -> str:
         ...
-        
+
     @abstractmethod
-    def id_resolver(self) -> str: 
+    def id_resolver(self) -> str:
         """Method to define how Document ID should be created"""
         ...
-    
-    @property   
+
+    @property
     def contents(self) -> str:
         """Return the text contents of a Document"""
         if not self._contents:
             self._contents = self.read()
-        
+
         return self._contents
-    
+
     @property
     def id(self):
         if not self._id:
             self._id = self.id_resolver()
-        
+
         return self._id
-    
+
     @property
     def metadata(self):
         """Return the metadata of a Document"""
+        if self._metadata:
+            return self._metadata
+
         metadata = self.metadata_processor(self.contents)
-        return DocumentMetadata(**metadata)
-    
+        self._metadata = DocumentMetadata(**metadata)
+        return self._metadata
+
     @property
-    def links(self):
+    def relations(self):
         """Return the Document ID of the links"""
-        sets_of_links = self.relations_processor(self.contents)
-        return 
-        
-    # @abstractmethod
-    # def create_datamodel(self):
-    #     ...
-        
-    
-#     text: str
-#     tags: List[str]
-#     terms: List[str]
-    
-    
+        if self._relations:
+            return self._relations
+
+        self._relations = self.relations_processor(self.contents)
+        return self._relations
+
+
 # @dataclass
 # class GraphDocumentModel(Document):
 
-    
+
 #     def return_document(self):
 #         return {
 #             'text': self.text,
-#             'tags': self.tags, 
+#             'tags': self.tags,
 #         }
-    
-    
-    
-
-    
-    
--- tests/providers/obsidian/test_processors.py
+++ tests/providers/obsidian/test_processors.py
@@ -18,13 +18,13 @@ def test_frontmatter_processor_no_meta(doc_without_frontmatter):
 
 def test_links_processor_hv_links(doc_with_links):
     expected = set(['document-id-1', 'document-id-2', 'document-id-3'])
-    result = links_processor(doc_with_links)
+    result = doc_with_links.relations
     assert result == expected
 
 
 def test_links_processor_no_links(doc_without_links):
     expected = set()
-    result = links_processor(doc_without_links)
+    result = doc_without_links.relations
     assert result == expected
 
     
