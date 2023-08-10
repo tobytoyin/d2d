0ac0cabca589639aaab2d3fdb5314f649551f91f
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-08-10 22:38:55 +0100

    remove cred

--- doc_graph_processor/db/neo4j_conn.py
+++ doc_graph_processor/db/neo4j_conn.py
@@ -14,19 +14,3 @@ class Neo4JConnector:
     #     # methods that could exec in finally block
     #     self.session.close()
     #     return True
-    
-
-
-
-n4j = Neo4JConnector("neo4j+s://ae48b625.databases.neo4j.io", "neo4j", "jqUuhsUmBYq7CA7GZ5avmT0DIzHJq-SL3nN1gYEYfcQ")
-n4j.driver.execute_query("CREATE (n:Person {name: 'Andy', title: 'Developer'})")
-
-n4j.driver.close()
-# with n4j.driver.session() as session: 
-#     print(session)
-#     result = session.execute_query()
-#     print(result)
-#     result.consume()
-#     print(result)
-# print(driver)
-# driver.close()  # close the driver object
