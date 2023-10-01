8f880094ae6ab996b3857782af465d5e4a797567
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-10-01 12:59:27 +0100

    fix app import

--- doc_uploader/api.py
+++ doc_uploader/api.py
@@ -1,5 +1,4 @@
-from app.logger import setup_custom_logger
-
+from doc_uploader.app.logger import setup_custom_logger
 from doc_uploader.connectors.factory import get_connector
 from doc_uploader.doc_handlers.factory import create_document
 from doc_uploader.services.db_handler.factory import get_uow
