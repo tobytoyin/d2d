2cb66f4c7992f5c1ccefb56123fd1c2f47a112d6
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-10-01 12:57:52 +0100

    add logger

--- doc_uploader/services/db_handler/uow/neo4j/handler.py
+++ doc_uploader/services/db_handler/uow/neo4j/handler.py
@@ -1,9 +1,13 @@
+import logging
+
 from doc_uploader.doc_handlers.interfaces import Document
 from doc_uploader.models.datamodels import GraphModel
-from doc_uploader.utils import no_quotes_object
+from doc_uploader.utils import invalid_key_fix, no_quotes_object
 
 from ...factory import DocToDBAdapters
 from ...protocols import DocumentToDB
+
+logger = logging.getLogger("root")
 
 
 class _Neo4JUoW:
@@ -13,20 +17,24 @@ class _Neo4JUoW:
         self.model = GraphModel(document)
 
     @property
-    def _id(self):
-        print(self.model.dict)
+    def node_id(self):
         return self.model.dict["id"]
 
     @property
     def _node_match_props(self) -> str:
-        return no_quotes_object({"id": self._id})
+        """cypher MATCH query for matching the node with this.node_id"""
+        return no_quotes_object({"id": self.node_id})
 
     @property
-    def _node_props(self) -> str:
-        obj = {"id": self._id, **self.model.dataobj.fields}
-        # change keys with "-" to "_"
-        obj = {k.replace("-", "_"): v for k, v in obj.items()}
-        return no_quotes_object(obj)
+    def _node_properties(self) -> str:
+        """Unpacking model's attribs into Cypher's node property syntax"""
+        props = {
+            "id": self.node_id,  # custom node id
+            "contents": self.model.dataobj.contents,  # document contents
+            **self.model.dataobj.fields,  # other optional documents metadata
+        }
+        props = invalid_key_fix(props, invalid_sym="-", valid_sym="_")
+        return no_quotes_object(props)
 
     @property
     def node_label(self) -> str:
@@ -39,7 +47,6 @@ class _Neo4JUoW:
     def create_dep_nodes(self, tx, rel_ids):
         for link_id in rel_ids:
             q = f"MERGE (n {no_quotes_object({'id': link_id})})"
-            print(q)
             tx.run(q)
 
     def create_one_to_many_rel(self, tx, rel_ids):
@@ -48,24 +55,33 @@ class _Neo4JUoW:
         MATCH (target) WHERE target.id IN {rel_ids}
         MERGE (n)-[:LINK]->(target)
         """
-        print(existing_nodes_query)
         tx.run(existing_nodes_query)
 
 
 @DocToDBAdapters.register(name="neo4j")
 class Neo4JUoW(_Neo4JUoW, DocumentToDB):
     def update_or_create_document(self, tx):
-        """create or update a node"""
+        """Create or update a node in Neo4J
+
+        This includes:
+        1. Merge with the Node with the same Node.id
+        2. Set the Node's label as entity_type
+        3. Set the Node's properties with new properties
+        4. Set the Node's lastEdited properties with updated timestamp
+
+        """
 
         query = f"""
         MERGE ( n {self._node_match_props} )
         SET 
             n:{self.node_label},
-            n = {self._node_props},
+            n = {self._node_properties},
             n.lastEdited = timestamp()         
         """
 
-        print(query)
+        logger.debug("neo4j - updating or creating new document")
+        logger.debug("query:\n%s", query)
+
         tx.run(query)
 
     def update_or_create_relationships(self, tx):
