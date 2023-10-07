21723a296b6358112f606cca1ca3c74fc7efac2a
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-10-07 17:29:51 +0100

    update neo4j access to graphDBModel

--- doc_uploader/services/db_handler/uow/mock/handler.py
+++ doc_uploader/services/db_handler/uow/mock/handler.py
@@ -1,22 +1,19 @@
 import logging
 
-from doc_uploader.models.datamodels import BaseDBModel
-
 from ...factory import DocToDBAdapters
 from ...protocols import DocumentToDB
 
+# @DocToDBAdapters.register(name="mock")
+# class MockHandler(DocumentToDB):
+#     def __init__(self, model: BaseDBModel) -> None:
+#         self.model = model
+#         super().__init__()
 
-@DocToDBAdapters.register(name="mock")
-class MockHandler(DocumentToDB):
-    def __init__(self, model: BaseDBModel) -> None:
-        self.model = model
-        super().__init__()
+#     def update_or_create_document(self):
+#         logging.debug(self.model.dataobj)
+#         logging.debug("updated and created document")
+#         return len(self.model.dict.keys())
 
-    def update_or_create_document(self):
-        logging.debug(self.model.dataobj)
-        logging.debug("updated and created document")
-        return len(self.model.dict.keys())
-
-    def update_or_create_relationships(self):
-        logging.debug(f"created relationships: {self.model.dataobj.relations}")
-        return len(self.model.dataobj.relations)
+#     def update_or_create_relationships(self):
+#         logging.debug(f"created relationships: {self.model.dataobj.relations}")
+#         return len(self.model.dataobj.relations)
