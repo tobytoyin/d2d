44419b6a7fe7eec60ecd980a2004630fff4567ac
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-11-12 23:27:24 +0000

    update provider and convertors to share the same generic interface

--- d2d/providers/factory.py
+++ d2d/providers/factory.py
@@ -8,9 +8,19 @@ _CATALOG = {
 }
 
 
+def get_provider(provider_name: str) -> type[SourceTextTasks] | None:
+    provider = _CATALOG.get(provider_name, None)
+    if provider is None:
+        logging.warning(
+            "'%s' does not exist",
+            provider_name,
+        )
+    return provider
+
+
 def get_task_fn(provider_name: str, task_name: str) -> TaskFunction | None:
     try:
-        provider = _CATALOG.get(provider_name)
+        provider = get_provider(provider_name=provider_name)
         return getattr(provider, task_name)
 
     except AttributeError:
--- d2d/providers/interface.py
+++ d2d/providers/interface.py
@@ -1,24 +1,14 @@
 from typing import Any, Callable, Generic, Literal, Protocol, TypeAlias, TypeVar
 
+from d2d.contracts.interfaces import ProviderInterface
 from d2d.contracts.payload import SourcePayloadDict
 
 T = TypeVar("T")
-TaskResultDict: TypeAlias = dict[Literal["result"], Any]
+TaskResultDict: TypeAlias = dict[str, Any]
 TaskFunction: TypeAlias = Callable[[T], TaskResultDict]
 
 
-class ProviderInterface(Protocol):
-    """
-    A Provider can implement a package by implement at least one type of Protocol
-    - SourceTextTasks - tasks that provides a text to result implementation
-    - SourceIO - tasks that provides a read to text implementation
-
-    :param Protocol: _description_
-    :type Protocol: _type_
-    """
-
-
-class SourceTextTasks(ProviderInterface):
+class SourceTextTasks(ProviderInterface[TaskFunction[str]]):
     """Interface for Storing Text-related Tasks
 
     :param Protocol: _description_
@@ -26,15 +16,15 @@ class SourceTextTasks(ProviderInterface):
     """
 
     # all function in this interface follows TaskSignature
-    summary = TaskFunction[str]
+    summary: TaskFunction[str]
 
 
-class SourceMetaTasks(ProviderInterface):
+class SourceMetaTasks(Protocol):
     """Responsible to convert source payload into internal IO
 
     :param Protocol: _description_
     :type Protocol: _type_
     """
 
-    source_text = Callable[[SourcePayloadDict], str]
-    uid_gen = Callable[[SourcePayloadDict], str]
+    source_text: Callable[[SourcePayloadDict], str]
+    uid_gen: Callable[[SourcePayloadDict], str]
