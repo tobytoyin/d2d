a62bf3e74f5aae4614e3312fb30e1b5b58527610
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-11-25 18:26:15 +0000

    update module paths

--- providers/obsidian/utils.py
+++
@@ -1,6 +0,0 @@
-def append_dict_iterable(target: dict, new: dict, append_keys) -> dict:
-    target = target.copy()
-    for key in append_keys:
-        target[key] += new[key]
-
-    return target
