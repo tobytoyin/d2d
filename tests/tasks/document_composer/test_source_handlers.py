1483010c78dd3dcb19ab50fbd980dc833b46b6dc
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-11-25 17:49:43 +0000

    update the SourceMetadata interface; refactor gen_uid as part of the metadata method (#13)

--- tests/tasks/document_composer/test_source_handlers.py
+++ tests/tasks/document_composer/test_source_handlers.py
@@ -2,8 +2,8 @@ import logging
 
 from d2d.contracts.payload import Source, SourceSpec
 from d2d.tasks.document_composer._source_handler import (
+    _get_source_metadata,
     _get_source_text,
-    _get_source_uid,
 )
 
 from ..payload_fixtures import *
@@ -22,5 +22,7 @@ def test_get_source_text():
 def test_source_uid():
     handler_payload = SourceSpec(provider="mock")
     source = Source(path="dummy.txt")  # type: ignore
-    reader = _get_source_uid(source, handler_payload)
-    assert reader == "dummy"
+    metadata = _get_source_metadata(source, handler_payload)
+    assert metadata["uid"] == "dummy"
+    assert len(metadata.keys()) == 2
+    logging.debug(metadata)
