1c8f1e96d0647042604495cdbeae16af7a87dd4f
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-12-24 14:01:00 +0000

    update pytoml

--- d2d/plugins/neo4j/to_embedding.py
+++ d2d/plugins/neo4j/to_embedding.py
@@ -1,24 +1,27 @@
-from d2d.contracts.documents import Document
+# from d2d.contracts.documents import Document
 
-from ..interface import DocumentPlugin
-from .mixin import GraphDBMixin
-from .utils import no_quotes_object
+# from ..document_to_db import _NodeIdentity
+# from ..interface import DocumentPlugin
+# from .mixin import GraphDBMixin
+# from .utils import no_quotes_object
 
+# # class DocumentToN4JEmbedding(DocumentPlugin, GraphDBMixin):
+# #     def update_or_create_embedding(self, document: Document):
+# #         match_self = _NodeIdentity.get_self(document)
+# #         embedding = no_quotes_object(document.embedding.prefix_model_dump())
 
-class DocumentToN4JEmbedding(DocumentPlugin, GraphDBMixin):
-    def update_or_create_embedding(self, document: Document):
-        embedding = no_quotes_object(document.embedding.prefix_model_dump())
-        match_self = no_quotes_object({"uid": document.uid})
+# #         new_props = {f"root.{k} = {v}" for k, v in embedding.items()}
+# #         query = f"""
+# #         WITH apoc.convert.fromJsonMap({repr(json_props)}) AS props
+# #         MERGE ( n {match_self} )
+# #         SET
+# #             n = props,
+# #             n:{_NodeIdentity.node_label(document)},
+# #             n.lastEdited = timestamp()
+# #         """
+# #         print(query)
+# #         tx = lambda tx: tx.run(query)
 
-        new_props = {f"root.{k} = {v}" for k, v in embedding.items()}
-        query = f"""
-        MERGE (root {match_self}
-        SET
-
-        """
-        print(query)
-        tx = lambda tx: tx.run(query)
-
-        with self.driver.session() as session:
-            _ = session.execute_write(tx)
-        self.close_driver()
+# #         with self.driver.session() as session:
+# #             _ = session.execute_write(tx)
+# #         self.close_driver()
