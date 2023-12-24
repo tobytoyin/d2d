79272f16ee720b81daaef58af6b03065f3a1095e
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-12-24 18:27:06 +0000

    Update __init__.py

--- d2d_providers/nlp/__init__.py
+++ d2d_providers/nlp/__init__.py
@@ -9,9 +9,11 @@ class TaskCatalog:
 
         sentences = text.split(sent_delim)
         embeddings = model.encode(sentences)
+        doc_emb = list(embeddings.mean(axis=0))
 
         return {
-            "vector": tuple(embeddings.mean(axis=0)),
+            "vector": doc_emb,
             "source": "sentence_transformers",
             "model": "all-MiniLM-L6-v2",
+            "dimension": len(doc_emb), 
         }
