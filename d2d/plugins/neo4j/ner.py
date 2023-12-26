5bd7a5ae2838015b12611b0d951605c78a25f5d5
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-12-26 16:59:39 +0000

    add links between document node to ner node (#39)

--- d2d/plugins/neo4j/ner.py
+++ d2d/plugins/neo4j/ner.py
@@ -1,5 +1,6 @@
-from d2d.contracts import documents as doc
 import json
+
+from d2d.contracts import documents as doc
 
 from .utils import no_quotes_object
 
@@ -8,14 +9,17 @@ def add_named_entites(tx, document: doc.Document):
     # add named entites nodes
     for entity in document.ner.entities:
         json_props = json.dumps({"id": entity.id, **entity.properties}, default=str)
-        match_self = no_quotes_object({"id": entity.id})
+        match_root = no_quotes_object({"id": entity.id})
+        match_doc = no_quotes_object({"uid": document.uid})
         query = f"""
         WITH apoc.convert.fromJsonMap({repr(json_props)}) AS props
-        MERGE ( n {match_self} )
+        MERGE ( n {match_root} )
         SET
             n = props,
             n:NERNode:{entity.type},
             n.lastEdited = timestamp()
+
+        MERGE (doc {match_doc})-[ :NERContain ]->(n)
         """
         print(query)
         tx.run(query)
@@ -25,7 +29,9 @@ def add_relations(tx, document: doc.Document):
     # add named entities relations links
     for rel in document.ner.relations:
         rel_props = no_quotes_object(rel.properties)
-        match_self = no_quotes_object({"id": rel.root})
+        match_root = no_quotes_object({"id": rel.root})
         match_other = no_quotes_object({"id": rel.target})
-        q = f"MERGE (root {match_self})-[ :{rel.type} {rel_props} ]->(other {match_other})"
+        q = f"""
+        MERGE (root {match_root})-[ :{rel.type} {rel_props} ]->(other {match_other})
+        """
         tx.run(q)
