9ab7a667e99622a76fbe94519a555208e75be81d
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-12-26 15:59:18 +0000

    disable nlp

--- d2d_providers/nlp/__init__.py
+++ d2d_providers/nlp/__init__.py
@@ -5,15 +5,16 @@ from sentence_transformers import SentenceTransformer
 class TaskCatalog:
     @staticmethod
     def embedding(text: str, sent_delim="\r"):
-        model = SentenceTransformer("all-MiniLM-L6-v2")
+        return
+        # model = SentenceTransformer("all-MiniLM-L6-v2")
 
-        sentences = text.split(sent_delim)
-        embeddings = model.encode(sentences)
-        doc_emb = list(embeddings.mean(axis=0))
+        # sentences = text.split(sent_delim)
+        # embeddings = model.encode(sentences)
+        # doc_emb = list(embeddings.mean(axis=0))
 
-        return {
-            "vector": doc_emb,
-            "source": "sentence_transformers",
-            "model": "all-MiniLM-L6-v2",
-            "dimension": len(doc_emb), 
-        }
+        # return {
+        #     "vector": doc_emb,
+        #     "source": "sentence_transformers",
+        #     "model": "all-MiniLM-L6-v2",
+        #     "dimension": len(doc_emb),
+        # }
