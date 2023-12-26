e3e1a6c1da7aa325af8b8d1475d7a56e67dea44c
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-12-26 15:49:18 +0000

    Ner (#35)
    * add ner into contracts
    
    * add ner into tasks interface
    
    * add ner into neo4j plusin

--- d2d/plugins/neo4j/__init__.py
+++ d2d/plugins/neo4j/__init__.py
@@ -6,3 +6,4 @@ doc_to_n4j = DocumentToNeo4J()
 class ServiceCatalog:
     update_or_create_linked_documents = doc_to_n4j.update_or_create_linked_documents
     delete_document = doc_to_n4j.delete_document
+    create_named_entities = doc_to_n4j.create_named_entities
---
+++ d2d/plugins/neo4j/ner.py
@@ -0,0 +1,19 @@
+from d2d.contracts import documents as doc
+
+from .utils import no_quotes_object
+
+
+def add_named_entites(tx, document: doc.Document):
+    # add named entites nodes
+    for entity in document.named_entity_relations.entites:
+        json_props = entity.model_dump(mode="json")
+        match_self = no_quotes_object({"id": entity.id})
+        query = f"""
+        WITH apoc.convert.fromJsonMap({repr(json_props)}) AS props
+        MERGE ( n {match_self} )
+        SET
+            n = props
+            n:{entity.type}
+            n.lastEdited = timestamp()
+        """
+        tx.run(query)
