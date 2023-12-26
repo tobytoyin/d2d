810049728321e4d6cfaaa3f0c060efe9d5f94546
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-12-26 16:06:42 +0000

    disable nlp

--- d2d/plugins/neo4j/ner.py
+++ d2d/plugins/neo4j/ner.py
@@ -17,3 +17,13 @@ def add_named_entites(tx, document: doc.Document):
             n.lastEdited = timestamp()
         """
         tx.run(query)
+
+
+def add_relations(tx, document: doc.Document):
+    # add named entities relations links
+    for rel in document.ner.relations:
+        rel_props = no_quotes_object(rel.properties)
+        match_self = no_quotes_object({"id": rel.root})
+        match_other = no_quotes_object({"id": rel.target})
+        q = f"MERGE (root {match_self})-[ :{rel.type} {rel_props} ]->(other {match_other})"
+        tx.run(q)
