e01a13b0e1dd64e3f26a4da9af82a6cb877047db
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-12-26 16:24:45 +0000

    Update ner.py

--- d2d/plugins/neo4j/ner.py
+++ d2d/plugins/neo4j/ner.py
@@ -7,16 +7,17 @@ from .utils import no_quotes_object
 def add_named_entites(tx, document: doc.Document):
     # add named entites nodes
     for entity in document.ner.entities:
-        json_props = json.dumps(entity.model_dump())
+        json_props = json.dumps(entity.model_dump(), default=str)
         match_self = no_quotes_object({"id": entity.id})
         query = f"""
         WITH apoc.convert.fromJsonMap({repr(json_props)}) AS props
         MERGE ( n {match_self} )
         SET
             n = props,
-            n:{entity.type},
+            n:NERNode:{entity.type},
             n.lastEdited = timestamp()
         """
+        print(query)
         tx.run(query)
 
 
@@ -26,5 +27,5 @@ def add_relations(tx, document: doc.Document):
         rel_props = no_quotes_object(rel.properties)
         match_self = no_quotes_object({"id": rel.root})
         match_other = no_quotes_object({"id": rel.target})
-        q = f"MERGE (root {match_self})-[ :{rel.type} {rel_props} ]->(other {match_other})"
+        q = f"MERGE (root {match_self})-[ :NERLink:{rel.type} {rel_props} ]->(other {match_other})"
         tx.run(q)
