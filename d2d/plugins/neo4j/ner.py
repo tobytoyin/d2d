0625a75c2ec4863075f4ad954ba84d54125b3838
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-12-26 17:15:23 +0000

    null escape for ner

--- d2d/plugins/neo4j/ner.py
+++ d2d/plugins/neo4j/ner.py
@@ -6,6 +6,9 @@ from .utils import no_quotes_object
 
 
 def add_named_entites(tx, document: doc.Document):
+    if document.ner.entities is None: 
+        return 
+    
     # add named entites nodes
     for entity in document.ner.entities:
         json_props = json.dumps({"id": entity.id, **entity.properties}, default=str)
@@ -27,6 +30,9 @@ def add_named_entites(tx, document: doc.Document):
 
 
 def add_relations(tx, document: doc.Document):
+    if document.ner.relations is None: 
+        return 
+    
     # add named entities relations links
     for rel in document.ner.relations:
         rel_props = no_quotes_object(rel.properties)
