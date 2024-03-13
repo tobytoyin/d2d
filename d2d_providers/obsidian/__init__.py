52c802a0c8dbd017740086e08837336a8276d6aa
Toby <40096033+tobytoyin@users.noreply.github.com>
2024-03-13 18:26:08 +0000

    Update __init__.py

--- d2d_providers/obsidian/__init__.py
+++ d2d_providers/obsidian/__init__.py
@@ -21,7 +21,7 @@ class SourceCatalog:
         
         return {
             "uid": str(d["path"]).split("/")[-1].split(".")[0],
-            "url": f"obsidian://open?vault=master-notes&file={unquote(d['path')}", 
+            "url": f"obsidian://open?vault=master-notes&file={unquote(d['path'])}", 
         }
 
 
